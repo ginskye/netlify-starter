952ed66eaa3eb027420c4bf7d8867ad2078fae02
Emily Hall <bibliomaniac@gmail.com>
2023-03-06 11:10:46 -0500

    add resume top to header

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,23 @@
 export default function Header({ title }) {
   return (
     <>
-      {/* <h1 className="title">{title}</h1>
+       <h1 className="title">{title}</h1>
       <p>
-        Hi 👋! This template gives you a{" "}
-        <a href="https://nextjs.org/">Next.js</a> app with the scaffolding for{" "}
-        <a href="https://www.netlify.com/products/functions/">
-          Netlify Functions
-        </a>
-        , <a href="https://www.netlify.com/products/forms/">Forms</a>, and{" "}
-        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
-        aim was to give you the code you would need to hit the ground running
-        with a few fun features.
+      <center><h2 class= "name">Emily Hall</h2></center>
+      <center>PITTSBURGH, PA 15221 | (412) 251-6277 | emily.jean.hall@proton.me | linkedin.com/in/emily-jean-hall | github.com/ginskye</center>
+   
       </p>
 
-      <p>
-        You can find the code for this project on GitHub at{" "}
-        <a href="https://github.com/netlify-templates/nextjs-toolbox">
-          https://github.com/netlify-templates/nextjs-toolbox
-        </a>
-        ! Happy coding!
-      </p> */}
+      
+    <p>
+    
+    <center>FULL STACK SOFTWARE DEVELOPER </center>
+Analytical and organized professional and developer with outstanding attention to detail and excellent communication skills.   Proven ability to quickly learn and navigate multiple informational systems and technologies.  Committed to teamwork, accountability and continuous improvement and refinement of problem-solving skills.
+
+    <strong>Technologies and Concepts:</strong>  C# | Ruby | SQL | MySQL | HTML | CSS | Javascript | Ruby on Rails | .Net with MVC | React and React Native | AngularJS | Android Studio | Python 3 | Selenium | Xamarin | Microsoft Office Suite - Outlook,  Excel, PowerPoint, Teams | Power BI | Azure DevOps | Ubuntu Linux | Element | Slack | IBM Z Mainframe | Object Oriented Programming |  Test Driven Development | Version Control (Git and Github) | APIs | Scrum and Agile methodology | IDEs including Visual Studio, Visual Studio Code, IntelliJ, SublimeText, SQLite Studio, PyCharm
+    
+    
+    </p>
     </>
   );
 }
